61975a5b218abb12185b033a10dccdaf20c6696f
Lucy Moore <lucy.moore@devacademy.co.nz>
2023-11-20 09:56:36 +1300

    reset routes.tsx to skeleton

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,10 +1,8 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
 import AppLayout from './components/AppLayout.tsx'
-import Fruits from './components/Fruits.tsx'
+//import components
 
 export const routes = createRoutesFromElements(
-  <Route path="/" element={<AppLayout />}>
-    <Route index element={<Fruits />} />
-  </Route>
+  <Route path="/" element={<AppLayout />}></Route>
 )
