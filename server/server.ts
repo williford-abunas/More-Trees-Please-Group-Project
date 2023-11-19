e92bd28ca6d6ef2f0194a724ad672b41cc89b362
Lucy Moore <lucy.moore@devacademy.co.nz>
2023-11-20 10:22:17 +1300

    server.ts updated

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import * as Path from 'node:path'
 import * as URL from 'node:url'
 
-import fruitRoutes from './routes/fruits.ts'
+//import your routes
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
@@ -12,7 +12,8 @@ const server = express()
 server.use(express.json())
 server.use(express.static(Path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
+//plant routes? bird routes?
+//server.use('/api/v1/', )
 
 server.get('*', (req, res) => {
   res.sendFile(Path.join(__dirname, 'public/index.html'))
