2bbda4a3781ac28e9216cbf42d52c14a27d54208
cody-ilg <143582504+cody-ilg@users.noreply.github.com>
2023-11-21 11:45:49 +1300

    Added placeholders to location route

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -5,22 +5,15 @@ import * as db from '../db/db.ts'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  try {
-    // const allBirds = await db.getAllBirds()
-    // Placeholder DB Functions, waiting DB Functions
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: `This isn't working!` })
-  }
-})
+router.get('/', (req, res) => {})
 
 //Location GETS
 
 router.get('/:location', async (req, res) => {
   const location = req.params.location
   try {
-    // const loctionBirds = await db.getBirdsByLocation(id)
+    // const allBirds = await db.getAllBirds()
+    // const regionTrees = await db.getRegionTrees
     // Placeholder DB Functions, waiting DB Functions
   } catch (error) {
     console.log(error)
