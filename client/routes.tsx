456ba5a5860124e1e73008146942bd226d05d5cf
lj-linessmith <lewis.lines.smith@gmail.com>
2023-11-21 11:57:37 +1300

    images displaying

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,10 +1,7 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
-import AppLayout from './components/AppLayout.tsx'
+import Home from './components/Home.tsx'
 
 export const routes = createRoutesFromElements(
-  <Route path="/" element={<AppLayout />}>
-    {/* <Route index element={< />} /> */}
-  </Route>
-
+  <Route path="/" element={<Home />}></Route>
 )
