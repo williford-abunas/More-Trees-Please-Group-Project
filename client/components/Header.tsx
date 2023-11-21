6780a45e38fb5076a83c3908cb9992f823cbcb6c
lj-linessmith <lewis.lines.smith@gmail.com>
2023-11-21 17:08:03 +1300

    hovering works

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,8 +1,7 @@
 const Header = () => {
   return (
     <div className="header-wrapper">
-      <h1>More Trees Please!</h1>
-      <p>Look! An empty barren land, lets regrow the native forrest!</p>
+      <h1> More Trees Please </h1>
     </div>
   )
 }
