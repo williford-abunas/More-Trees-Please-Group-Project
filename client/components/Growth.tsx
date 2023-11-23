dea8ab60ed3e92f1c6048980b7868f5b99a0d72c
Raidon Ramiah <142761066+raidon-ramiah@users.noreply.github.com>
2023-11-23 18:28:36 +1300

    Add start of event log

--- client/components/Growth.tsx
+++ client/components/Growth.tsx
@@ -7,11 +7,9 @@ export async function checkGrowth() {
 
   seedArray.forEach((seed) => {
     const currentTime = new Date().getTime()
-    console.log('this is the current time:' + currentTime)
-    console.log('this is the seeds time: ' + seed.timestamp)
     const timeDifference = currentTime - seed.timestamp
 
-    if (timeDifference >= 1000) {
+    if (timeDifference >= 15000) {
       seed.isMature = true
       makeMature(seed)
       console.log('this function is being called in the if statement')
